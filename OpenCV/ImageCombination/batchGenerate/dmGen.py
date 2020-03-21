9975468e14876da239907f7f5b1e4d1338f2c025
raghavddps2 <raghav.ddps2@gmail.com>
2020-03-22 01:05:50 +0530

    Adding some scan stuff

--- OpenCV/ImageCombination/batchGenerate/dmGen.py
+++ OpenCV/ImageCombination/batchGenerate/dmGen.py
@@ -2,10 +2,11 @@ import cv2
 import os
 import shutil
 import treepoem
+from pystrich.datamatrix import DataMatrixEncoder
 def dmGen(i):
+    # image = DataMatrixEncoder("This is a DataMatrix.")
     image = treepoem.generate_barcode(barcode_type='datamatrix', data="8351:5684:1254:1234:1245:"+str(i).zfill(4))
     filename = "dm"+str(i).zfill(4)+".png"
     image.save("dm/"+filename)
-    #shutil.move(filename, "batch/images/"+filename)
     qr = cv2.imread("dm/"+filename)
     return qr
